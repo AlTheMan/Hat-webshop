30891aed7fe365f957dd34b9ab04d3533978d689
Algot von Reybekiel <algot98@hotmail.com>
2023-09-29 17:02:52 +0200

    Switch för servlet

--- src/main/java/com/example/Dist_sys_lab1_webshop/UI/ControllerServlet.java
+++ src/main/java/com/example/Dist_sys_lab1_webshop/UI/ControllerServlet.java
@@ -10,7 +10,14 @@ import jakarta.servlet.annotation.*;
  * All interaktion med JSP-sidorna ska gå via denna
  *
  */
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+
+
+
+
+@WebServlet(name = "helloServlet", value = {
+        "/hello-servlet",
+        "/add-item-servlet",
+        "/remove-item-servlet"})
 public class ControllerServlet extends HttpServlet {
     private String message;
     private String message_buy_item;
@@ -23,6 +30,26 @@ public class ControllerServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        String path = request.getServletPath(); // Hämta sökvägen för den aktuella begäran
+        PrintWriter out = response.getWriter();
+
+        //här kan vi ha en switch sats:
+        if ("/hello-servlet".equals(path)) {
+            hello_servlet(response);
+        } else if ("/add-item-servlet".equals(path)) {
+            out.println("<h1>Item Added!</h1>");
+        } else if ("/remove-item-servlet".equals(path)) {
+            out.println("<h1>Item Removed!</h1>");
+        }
+
+
+
+    }
+
+    public void destroy() {
+    }
+
+    private void hello_servlet(HttpServletResponse response) throws IOException {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
@@ -30,7 +57,5 @@ public class ControllerServlet extends HttpServlet {
         out.println("<h1>" + message_buy_item + "</h1>");
         out.println("</body></html>");
     }
-
-    public void destroy() {
-    }
 }
+
