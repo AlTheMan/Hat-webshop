cf37a254b5c8afcc76e8445d3a90cc4bc542aab1
AlTheMan <algot98@hotmail.com>
2023-10-02 11:35:11 +0200

    test
    test

--- src/main/java/com/example/Dist_sys_lab1_webshop/Database/ItemDB.java
+++ src/main/java/com/example/Dist_sys_lab1_webshop/Database/ItemDB.java
@@ -2,9 +2,9 @@ package com.example.Dist_sys_lab1_webshop.Database;
 
 import com.example.Dist_sys_lab1_webshop.Model.Item.Item;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Collection;
+import java.util.List;
 
 public class ItemDB extends Item {
 
@@ -15,9 +15,31 @@ public class ItemDB extends Item {
 
 	public static Collection<Item> getItemsFromDb() throws SQLException {
 		DBManager manager = DBManager.getInstance();
-		manager.getConnection().createStatement();
+		Connection connection = manager.getConnection();
 
-		
+		// Create a statement to execute SQL queries
+		Statement statement = connection.createStatement();
+
+		// Define your SQL query
+		String query = "SELECT id, name, description FROM Item"; // Assuming the table name is 'Item'
+
+		// Execute the query
+		ResultSet rs = statement.executeQuery(query);
+
+		// Process the results and create Item objects
+		List<Item> items = new ArrayList<>();
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			String description = rs.getString("description");
+			items.add(new Item(id, name, description));
+		}
+
+		// Clean up resources
+		rs.close();
+		statement.close();
+
+		return items;
 	}
 
 
